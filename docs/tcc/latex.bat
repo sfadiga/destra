a4b1c1b6b17a9583ce32ae8eab4592e5be7647ec
sandro <sfadiga@gmail.com>
2025-11-22 15:16:22 -0300

    Atualiza o TCC utilizando o template tex correto da USP-EESC

---
+++ docs/tcc/latex.bat
@@ -0,0 +1,15 @@
+perl.exe c:\texlive\2025\texmf-dist\scripts\latexmk\latexmk.pl -f -synctex=1 -interaction=nonstopmode -file-line-error -pdf -outdir=d:/Sandro/OneDrive/1_Estudos/CursoUSP/projetos/destra/docs/tcc d:/Sandro/OneDrive/1_Estudos/CursoUSP/projetos/destra/docs/tcc/tcc_sandro_fadiga
+del *.aux
+del *.loq
+del *.gz
+del *.bbl
+del *.blg
+del *.idx
+del *.toc
+del *.lot
+del *.lof
+del *.fls
+rem del *.log
+del *.ilg
+del *.ind
+del *.fdb_latexmk
