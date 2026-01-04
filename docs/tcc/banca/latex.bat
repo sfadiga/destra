80cf22f53afe380378195b4b56d17b27fbf12a90
sandro <sfadiga@gmail.com>
2026-01-04 13:56:03 -0300

    correção enviada para a banca

---
+++ docs/tcc/banca/latex.bat
@@ -0,0 +1,20 @@
+perl.exe c:\texlive\2025\texmf-dist\scripts\latexmk\latexmk.pl -f -synctex=1 -interaction=nonstopmode -file-line-error -pdf -outdir="." respostas_correcoes
+rem  pdflatex tcc_apresentacao.tex
+del *.aux
+del *.loq
+del *.gz
+del *.bbl
+del *.blg
+del *.idx
+del *.toc
+del *.lot
+del *.lof
+del *.fls
+del *.log
+del *.ilg
+del *.ind
+del *.fdb_latexmk
+del *.nav
+del *.out
+del *.snm
+del *.vrb
